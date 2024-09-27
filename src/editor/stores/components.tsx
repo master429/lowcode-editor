5fe7816324b81e63cfe39688a078d3b920be2c6d
liuqingpo <1432003391@qq.com>
2024-09-27 20:24:01 +0800

    feat: 定义核心数据store

---
+++ src/editor/stores/components.tsx
@@ -0,0 +1,95 @@
+import { create } from "zustand";
+
+export interface Component {
+  id: number;
+  name: string;
+  props: any;
+  children?: Component[];
+  parentId?: number;
+}
+
+interface State {
+  components: Component[]; //store 里保存着 components 组件树，它是一个用 children 属性连接起来的树形结构
+}
+
+interface Action {
+  addComponent: (component: Component, parentId?: number) => void;
+  deleteComponent: (componentId: number) => void;
+  updateComponentProps: (componentId: number, props: any) => void;
+}
+
+export const useComponetsStore = create<State & Action>((set, get) => ({
+  components: [
+    {
+      id: 1,
+      name: "Page",
+      props: {},
+      desc: "页面",
+    },
+  ],
+  addComponent: (component, parentId) =>
+    set((state) => {
+      if (parentId) {
+        const parentComponent = getComponentById(parentId, state.components);
+
+        if (parentComponent) {
+          if (parentComponent.children) {
+            parentComponent.children.push(component);
+          } else {
+            parentComponent.children = [component];
+          }
+        }
+
+        component.parentId = parentId;
+        return { components: [...state.components] };
+      }
+      return { components: [...state.components, component] };
+    }),
+  deleteComponent: (componentId) => {
+    if (!componentId) return;
+
+    const component = getComponentById(componentId, get().components);
+    if (component?.parentId) {
+      const parentComponent = getComponentById(
+        component.parentId,
+        get().components
+      );
+
+      if (parentComponent) {
+        parentComponent.children = parentComponent?.children?.filter(
+          (item) => item.id !== +componentId
+        );
+
+        set({ components: [...get().components] });
+      }
+    }
+  },
+  updateComponentProps: (componentId, props) =>
+    set((state) => {
+      const component = getComponentById(componentId, state.components);
+      if (component) {
+        component.props = { ...component.props, ...props };
+
+        return { components: [...state.components] };
+      }
+
+      return { components: [...state.components] };
+    }),
+}));
+
+// 树形结构，想要增删改都要先找到 parent 节点，先实现了查找方法
+export function getComponentById(
+  id: number | null,
+  components: Component[]
+): Component | null {
+  if (!id) return null;
+
+  for (const component of components) {
+    if (component.id == id) return component;
+    if (component.children && component.children.length > 0) {
+      const result = getComponentById(id, component.children); //树形结构中查找节点，自然是通过递归
+      if (result !== null) return result;
+    }
+  }
+  return null;
+}
