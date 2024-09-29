652a2595ea3d0b27bb04defc8be719df27d73efe
liuqingpo <1432003391@qq.com>
2024-09-29 13:23:56 +0800

    feat: 实现自定义js

--- src/editor/components/Prview/index.tsx
+++ src/editor/components/Prview/index.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { useComponentConfigStore } from "../../stores/component-config";
 import { Component, useComponetsStore } from "../../stores/components";
 import { message } from "antd";
-import { GoToLinkConfig } from "../Setting/actions/GoToLink";
-import { ShowMessageConfig } from "../Setting/actions/ShowMessage";
+import { ActionConfig } from "../Setting/ActionModal";
 
 export function Preview() {
   const { components } = useComponetsStore();
@@ -17,19 +16,26 @@ export function Preview() {
 
       if (eventConfig) {
         props[event.name] = () => {
-          eventConfig?.actions?.forEach(
-            (action: GoToLinkConfig | ShowMessageConfig) => {
-              if (action.type === "goToLink") {
-                window.location.href = action.url;
-              } else if (action.type === "showMessage") {
-                if (action.config.type === "success") {
-                  message.success(action.config.text);
-                } else if (action.config.type === "error") {
-                  message.error(action.config.text);
-                }
+          eventConfig?.actions?.forEach((action: ActionConfig) => {
+            if (action.type === "goToLink") {
+              window.location.href = action.url;
+            } else if (action.type === "showMessage") {
+              if (action.config.type === "success") {
+                message.success(action.config.text);
+              } else if (action.config.type === "error") {
+                message.error(action.config.text);
               }
+            } else if (action.type === "customJS") {
+              const func = new Function("context", action.code);
+              func({
+                name: component.name,
+                props: component.props,
+                showMessage(content: string) {
+                  message.success(content);
+                },
+              });
             }
-          );
+          });
         };
       }
     });
--- src/editor/components/Setting/ActionModal.tsx
+++ src/editor/components/Setting/ActionModal.tsx
@@ -1,21 +1,35 @@
 import { Modal, Segmented } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GoToLink, GoToLinkConfig } from "./actions/GoToLink";
 import { ShowMessage, ShowMessageConfig } from "./actions/ShowMessage";
+import { CustomJS, CustomJSConfig } from "./actions/CustomJS";
 
-interface ActionModalProps {
+export type ActionConfig = GoToLinkConfig | ShowMessageConfig | CustomJSConfig;
+
+export interface ActionModalProps {
   visible: boolean;
-  handleOk: (config?: GoToLinkConfig | ShowMessageConfig) => void;
+  action?: ActionConfig;
+  handleOk: (config?: ActionConfig) => void;
   handleCancel: () => void;
 }
 
 export function ActionModal(props: ActionModalProps) {
-  const { visible, handleOk, handleCancel } = props;
+  const { visible, action, handleOk, handleCancel } = props;
+
+  const map = {
+    goToLink: "访问链接",
+    showMessage: "消息提示",
+    customJS: "自定义 JS",
+  };
 
   const [key, setKey] = useState<string>("访问链接");
-  const [curConfig, setCurConfig] = useState<
-    GoToLinkConfig | ShowMessageConfig
-  >();
+  const [curConfig, setCurConfig] = useState<ActionConfig>();
+
+  useEffect(() => {
+    if (action?.type) {
+      setKey(map[action.type]);
+    }
+  }, [action]);
 
   return (
     <Modal
@@ -36,6 +50,8 @@ export function ActionModal(props: ActionModalProps) {
         />
         {key === "访问链接" && (
           <GoToLink
+            key="goToLink"
+            value={action?.type === "goToLink" ? action.url : ""}
             onChange={(config) => {
               setCurConfig(config);
             }}
@@ -43,6 +59,17 @@ export function ActionModal(props: ActionModalProps) {
         )}
         {key === "消息提示" && (
           <ShowMessage
+            key="showMessage"
+            value={action?.type === "showMessage" ? action.config : undefined}
+            onChange={(config) => {
+              setCurConfig(config);
+            }}
+          />
+        )}
+        {key === "自定义 JS" && (
+          <CustomJS
+            key="customJS"
+            value={action?.type === "customJS" ? action.code : ""}
             onChange={(config) => {
               setCurConfig(config);
             }}
